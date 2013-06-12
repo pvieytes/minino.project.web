8b8296105906903b61cbf85ebe3566e8d25d9359
Pablo Vieytes <pvieytes@openshine.com>
2013-06-12 12:01:38 +0200

    add some text

--- src/minino_web.erl
+++ src/minino_web.erl
@@ -9,7 +9,10 @@
 
 %% views
 -export([home_view/3,
-	 test_view/3]).
+	 download_view/3,
+	 start_view/3,
+	 code_view/3
+	]).
 
 %% minino funs
 
@@ -18,19 +21,28 @@ init(_MConf) ->
 
 dispatch_rules() ->
     [%% {Id::atom(), Path::[string()|atom()], view::atom()}
-     {root_page, [], home_view},
-     {home_page, ["home"], home_view},
-     {test_page, ["test", testvalue], test_view}
+     {root_page, [], home_view},  
+     {get_page, ["getting-started"], start_view},
+     {download_page, ["download"], download_view},
+     {code_page, ["code"], code_view}
     ].
 
 
 %% views
 
 home_view(MReq, _Args, _Term) ->
-    {ok, Html} = minino_api:render_template("home.html", [{text, "Meow!!"}]),
+    {ok, Html} = minino_api:render_template("home.html", []),
     minino_api:response(Html, MReq).
 
-test_view(MReq, Args, _Term) ->
-    TestVal = proplists:get_value(testvalue, Args),
-    Html = lists:flatten(io_lib:format("<html><body>test: ~s</body></html>", [TestVal])),
+start_view(MReq, _Args, _Term) ->
+    {ok, Html} = minino_api:render_template("getting-started.html", []),
     minino_api:response(Html, MReq).
+
+download_view(MReq, _Args, _Term) ->
+    {ok, Html} = minino_api:render_template("download.html", []),
+    minino_api:response(Html, MReq).
+
+code_view(MReq, _Args, _Term) ->
+    {ok, Html} = minino_api:render_template("code.html", []),
+    minino_api:response(Html, MReq).
+
